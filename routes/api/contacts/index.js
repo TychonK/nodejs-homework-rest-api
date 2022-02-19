cd6fe6098424821ef8c8b0b4f795c60732a95148
Tychon <tisha.kruglyak@gmail.com>
2022-02-19 17:48:22 +0100

    added: registration, login, logout, guard middleware

--- routes/api/contacts/index.js
+++ routes/api/contacts/index.js
@@ -2,37 +2,39 @@ import { Router } from 'express'
 import repositoryContacts from '../../../repository/contacts'
 import { validateCreate, validateUpdate, validateId, validateFavorite } from './validation'
 
+import guard from '../../../middlewares/guard' 
+
 const router = new Router()
 
-router.get('/', async (req, res, next) => {
+router.get('/', guard, async (req, res, next) => {
   const contacts = await repositoryContacts.listContacts()
   res.status(200).json( contacts )
 })
 
-router.get('/:id', validateId, async (req, res, next) => {
+router.get('/:id', [guard, validateId], async (req, res, next) => {
   const { id } = req.params
   const contact = await repositoryContacts.getContactById(id)
   contact ? res.status(200).json(contact) : res.status(404).json({ message: "Not found" })
 })
 
-router.post('/', validateCreate, async (req, res, next) => {
+router.post('/', [guard, validateCreate], async (req, res, next) => {
   const newContact = await repositoryContacts.addContact(req.body)
   res.status(201).json(newContact)
 })
 
-router.delete('/:id', validateId, async (req, res, next) => {
+router.delete('/:id', [guard, validateId], async (req, res, next) => {
   const { id } = req.params
   const contact = await repositoryContacts.removeContact(id)
   contact ? res.status(200).json({message: "contact deleted"}) : res.status(404).json({ message: "Not Found" })
 })
 
-router.put('/:id', validateId, validateUpdate, async (req, res, next) => {
+router.put('/:id', [guard, validateId, validateUpdate], async (req, res, next) => {
   const { id } = req.params
   const contact = await repositoryContacts.updateContact(id, req.body)
   contact ? res.status(200).json(contact) : res.status(404).json({ message: "Not Found" })
 })
 
-router.patch('/:id/favorite', validateId, validateFavorite, async (req, res, next) => {
+router.patch('/:id/favorite', [guard, validateId, validateFavorite], async (req, res, next) => {
   const { id } = req.params
   const contact = await repositoryContacts.updateFavorite(id, req.body)
   contact ? res.status(200).json( contact ) : res.status(404).json({message: "Not found"})
