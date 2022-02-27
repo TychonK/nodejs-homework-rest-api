7f1c131bc787903d07f8d5e851080823805e9c28
Tychon <tisha.kruglyak@gmail.com>
2022-02-27 16:34:47 +0100

    authorization validations added

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -3,10 +3,12 @@ import { registration, login, logout } from '../../../controllers/users'
 import { } from './validation'
 import guard from '../../../middlewares/guard'
 
+import { validateLogin, validateSignup } from './validation'
+
 const router = new Router()
 
-router.post('/signup', registration)
-router.post('/login', login)
+router.post('/signup', validateSignup, registration)
+router.post('/login', validateLogin, login)
 router.post('/logout', guard, logout)
 
 export default router
